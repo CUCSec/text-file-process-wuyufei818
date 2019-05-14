42415c6ebb27035059e142e6a470375600cb077d
wuyufff <1781978930@qq.com>
2019-05-14 10:04:37 +0800

    初次提交

--- wuyufei.py
+++ wuyufei.py
@@ -1,6 +1,7 @@
 import re
 count=0
 filepointer=open("log_files/201811113009.log","r")
-for line in filepointer.readlines()
+for line in filepointer.readlines():
     if(re.split("[0-9]{12}",line)):
         count=count+1
+    print(count)
