1f2413709c72e6b40e4b05e1d0c735093de2af73
wuyufff <1781978930@qq.com>
2019-05-24 17:02:10 +0800

    最后一次修改 ✌️✌️✌️

--- wuyufei.py
+++ wuyufei.py
@@ -1,7 +1,8 @@
-import re
-count=0
-filepointer=open("log_files/201811113009.log","r")
-for line in filepointer.readlines():
-    if(re.split("[0-9]{12}",line)):
-        count=count+1
-    print(count)
+with open("log_files/201811113009.log", encoding='utf8') as filepointer:
+    count=0
+    for line in filepointer :
+        line1 = line.split(',')[1]
+        line2 = line1.split('：')[1]
+        if  line2=='201811113009' :
+            count = count+1
+print(count)
